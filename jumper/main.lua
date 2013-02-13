e1bfaca5dc2b4915d759a7fe884bd5b65e8becd3
thw <exulceratio@web.de>
2013-02-13 11:11:58 +0100

    jumper fertsch

---
+++ jumper/main.lua
@@ -0,0 +1,71 @@
+STRICT = true
+DEBUG = true
+
+require 'zoetrope'
+
+Player = Animation:extend
+{
+    width = 32,
+    height = 48,
+    image = 'player.png',
+	sequences = 
+	{
+		right = { frames = {1, 2, 3, 4, 5, 6}, fps = 10 },
+		left = { frames = {7, 8, 9, 10, 11, 12}, fps = 10}
+	},
+	
+    acceleration = { y = 600 },
+    canJump = false,
+ 
+    onUpdate = function (self)
+        if the.keys:pressed('left') then
+            self.velocity.x = -100
+			self:play('left')
+        elseif the.keys:pressed('right') then
+            self.velocity.x = 100
+			self:play('right')
+        else
+            self.velocity.x = 0
+			self:freeze()
+        end
+ 
+        if the.keys:justPressed(' ') and self.canJump then
+		playSound('Jump6.wav')
+            self.velocity.y = -500
+            self.canJump = false
+        end
+    end,
+ 
+    onCollide = function (self)
+        if self.velocity.y > 0 then
+            self.velocity.y = 0
+            self.canJump = true
+        end
+    end
+}
+		
+Platform = Tile:extend
+{
+	width  = 128, height = 32,
+	image = 'brick.png',
+			
+	onCollide = function (self, other)
+		self:displace(other)
+	end
+}
+
+the.app = App:new
+{
+	onRun = function(self)
+		self.player = Player:new{x = 0, y = 0}
+		self:add(self.player)
+		self.platforms = Group:new()
+		self.platforms:add(Platform:new{ x = 0, y = 400 })
+		self.platforms:add(Platform:new{ x = 250, y = 400})
+		self:add(self.platforms)
+	end,
+	
+	onUpdate = function(self)
+		self.platforms:collide(self.player)
+	end
+}
